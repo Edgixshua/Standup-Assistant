137d0df86a1586837ddef8b9de1d3f1f3fbdd5cd
Joshua Sheffield <edgixshua@gmail.com>
2019-02-16 17:02:07 +0000

    Create initial solution with GIT functionality

---
+++ Standup-Assistant/Standup-Assistant/Functionality/GitFunctionality.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LibGit2Sharp;
+
+namespace Standup_Assistant.Functionality
+{
+    public static class GitFunctionality
+    {
+        public static IEnumerable<Commit> GetFilteredCommitLog(Repository repo, string branch)
+        {
+            var dateSince = new DateTimeOffset(DateTime.Now.AddDays(-1));
+            var dateUntil = new DateTimeOffset(DateTime.Now);
+
+            var filter = new CommitFilter
+            {
+                SortBy = CommitSortStrategies.Reverse | CommitSortStrategies.Time,
+                ExcludeReachableFrom = repo.Branches[branch].Tip,
+                IncludeReachableFrom = repo.Head.Tip
+            };
+
+            var results = repo.Commits.QueryBy(filter);
+
+            // Filter commit log to only return commits since yesterday.
+            var filteredLog = results.Where(commit => commit.Committer.When > dateSince && commit.Committer.When < dateUntil);
+
+            return filteredLog;
+        }
+
+        public static void WriteCommits(IEnumerable<Commit> commits)
+        {
+            foreach (var commit in commits)
+            {
+                Console.WriteLine(commit.Message);
+            }
+        }
+    }
+}
